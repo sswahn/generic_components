812c9ffad5e2954c905b28ce48a9fea76ecbb13e
sswahn <36422765+sswahn@users.noreply.github.com>
2021-07-08 16:09:18 -0400

    article styling

--- src/components/content/Article.js
+++ src/components/content/Article.js
@@ -1,19 +1,37 @@
 import styles from './article.module.css'
 
-export default () => {
+export default ({ 
+  avatar_src,
+  username,
+  date_time,
+  parsed_date,
+  body,
+  img_src,
+  img_alt,
+  video_src,
+  video_id
+ }) => {
 
   return (
     <article className={styles.article}>
       <header className={styles.header}>
-        <img src="" alt="" />
-        <h2>Username</h2>
-        <time datetime="">1 hour ago</time>
-        {/* dropdown menu */}
+        <div>
+          <img src={avatar_src} alt={username} />
+          <div>
+            <h2>{username}my name</h2>
+            <time datetime={date_time}>{parsed_date}time</time>
+          </div>
+        </div>
+        <div>
+          <button>
+            <span>&#8943;</span>
+          </button>
+        </div>
       </header>
       <div className={styles.main}>
-        <p>main content text.</p>
-        <img src="" alt="" />
-        <video controls="controls" xWebkitAirplay="allow" dataYoutubeId="N9oxmRT2YWw" src=""></video>
+        <p>{body}Hello.</p>
+        {img_src && <img src={img_src} alt={img_alt} />}
+        {video_src && <video controls="controls" src={video_src} dataYoutubeId={video_id} ></video>}
       </div>
       <footer className={styles.footer}>
         <button>Comment</button>
